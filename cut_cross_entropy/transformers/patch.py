0ee2ae302357f23709bfca6c60bad1368177b21f
NanoCode012 <nano@axolotl.ai>
2025-08-26 18:11:07 +0700

    feat: add seed_oss and sort refactor (#18)
    feat: add seed_oss and refactor

--- cut_cross_entropy/transformers/patch.py
+++ cut_cross_entropy/transformers/patch.py
@@ -1,29 +1,28 @@
 # Copyright (C) 2024 Apple Inc. All Rights Reserved.
 from typing import overload
 
-from transformers import PreTrainedModel, PretrainedConfig
+from transformers import PretrainedConfig, PreTrainedModel
 
 from cut_cross_entropy.cce_utils import LinearCrossEntropyImpl
 from cut_cross_entropy.linear_cross_entropy import LCE_IMPL_DEFAULT
 
+from .arcee import patch_arcee
 from .cohere import patch_cohere, patch_cohere2
+from .deepseek_v3 import patch_deepseek_v3
 from .gemma import patch_gemma
 from .gemma3 import patch_gemma2, patch_gemma3, patch_gemma3_text
-
-try:
-    from .gemma3n import patch_gemma3n, patch_gemma3n_text
-except ImportError:
-    patch_gemma3n = None  # type: ignore
-    patch_gemma3n_text = None  # type: ignore
-
+from .gemma3n import patch_gemma3n, patch_gemma3n_text
 from .glm4 import patch_glm, patch_glm4, patch_glm4_moe
+from .gpt_oss import patch_gpt_oss
+from .granite import patch_granite
+from .granitemoe import patch_granitemoe
+from .hunyuan_v1 import patch_hunyuan_v1_dense, patch_hunyuan_v1_moe
 from .llama import patch_llama
 from .llama4 import patch_llama4, patch_llama4_text
 from .mistral import patch_mistral
 from .mistral3 import patch_mistral3
-from .voxtral import patch_voxtral
+from .mixtral import patch_mixtral
 from .mllama import patch_mllama
-from .smollm3 import patch_smollm3
 from .phi import patch_phi
 from .phi3 import patch_phi3
 from .phi4_multimodal import patch_phi4_multimodal
@@ -33,55 +32,55 @@ from .qwen2_moe import patch_qwen2_moe
 from .qwen2_vl import patch_qwen2_vl
 from .qwen3 import patch_qwen3
 from .qwen3_moe import patch_qwen3_moe
-from .granite import patch_granite
-from .granitemoe import patch_granitemoe
-from .hunyuan_v1 import patch_hunyuan_v1_dense, patch_hunyuan_v1_moe
-from .arcee import patch_arcee
-from .mixtral import patch_mixtral
-from .gpt_oss import patch_gpt_oss
-from .deepseek_v3 import patch_deepseek_v3
-
+from .smollm3 import patch_smollm3
 from .utils import PatchOptions, TransformersModelT
+from .voxtral import patch_voxtral
+
+try:
+    from .seed_oss import patch_seed_oss
+except ImportError:
+    patch_seed_oss = None
 
 AXOLOTL_CCE_FORK = 1
 
 PATCH_FNS = {
-    "llama": patch_llama,
-    "llama4": patch_llama4,
-    "llama4_text": patch_llama4_text,
-    "mllama": patch_mllama,
-    "phi": patch_phi,
-    "phi3": patch_phi3,
-    "phi4_multimodal": patch_phi4_multimodal,
+    "arcee": patch_arcee,
+    "cohere": patch_cohere,
+    "cohere2": patch_cohere2,
+    "deepseek_v3": patch_deepseek_v3,
     "gemma": patch_gemma,
     "gemma2": patch_gemma2,
     "gemma3": patch_gemma3,
     "gemma3_text": patch_gemma3_text,
     "gemma3n": patch_gemma3n,
     "gemma3n_text": patch_gemma3n_text,
+    "glm": patch_glm,
+    "glm4": patch_glm4,
+    "glm4_moe": patch_glm4_moe,
+    "gpt_oss": patch_gpt_oss,
+    "granite": patch_granite,
+    "granitemoe": patch_granitemoe,
+    "hunyuan_v1_dense": patch_hunyuan_v1_dense,
+    "hunyuan_v1_moe": patch_hunyuan_v1_moe,
+    "llama": patch_llama,
+    "llama4": patch_llama4,
+    "llama4_text": patch_llama4_text,
     "mistral": patch_mistral,
     "mistral3": patch_mistral3,
     "mixtral": patch_mixtral,
-    "voxtral": patch_voxtral,
-    "smollm3": patch_smollm3,
+    "mllama": patch_mllama,
+    "phi": patch_phi,
+    "phi3": patch_phi3,
+    "phi4_multimodal": patch_phi4_multimodal,
     "qwen2": patch_qwen2,
     "qwen2_moe": patch_qwen2_moe,
     "qwen2_vl": patch_qwen2_vl,
     "qwen2_5_vl": patch_qwen2_5_vl,
     "qwen3": patch_qwen3,
     "qwen3_moe": patch_qwen3_moe,
-    "cohere": patch_cohere,
-    "cohere2": patch_cohere2,
-    "glm": patch_glm,
-    "glm4": patch_glm4,
-    "glm4_moe": patch_glm4_moe,
-    "granite": patch_granite,
-    "granitemoe": patch_granitemoe,
-    "hunyuan_v1_dense": patch_hunyuan_v1_dense,
-    "hunyuan_v1_moe": patch_hunyuan_v1_moe,
-    "arcee": patch_arcee,
-    "gpt_oss": patch_gpt_oss,
-    "deepseek_v3": patch_deepseek_v3,
+    "smollm3": patch_smollm3,
+    "seed_oss": patch_seed_oss,
+    "voxtral": patch_voxtral,
 }
 
 
---
+++ cut_cross_entropy/transformers/seed_oss.py
@@ -0,0 +1,50 @@
+"""Seed_OSS CCE patch. Inherits Llama. Adapted from transformers Seed_OSS PR."""
+
+# Copyright (C) 2024 Apple Inc. All Rights Reserved.
+
+# Copyright 2024 Axolotl AI. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from types import MethodType
+
+import transformers
+
+from cut_cross_entropy.transformers.utils import (
+    PatchOptions,
+    TransformersModelT,
+)
+
+
+def patch_seed_oss(
+    maybe_model: TransformersModelT | str | transformers.PretrainedConfig,
+    patch_options: PatchOptions,
+) -> TransformersModelT | None:
+    # Set the _PATCH_OPTS in the llama patch file
+    from . import llama as llama_patch
+
+    llama_patch._PATCH_OPTS = patch_options
+
+    cce_forward = llama_patch.cce_forward
+
+    from transformers.models.seed_oss import modular_seed_oss
+
+    if isinstance(maybe_model, transformers.PreTrainedModel):
+        assert isinstance(maybe_model, modular_seed_oss.SeedOssForCausalLM), (
+            f"Expected a SeedOssForCausalLM model. Got {type(maybe_model)}."
+        )
+        maybe_model.forward = MethodType(cce_forward, maybe_model)
+        return maybe_model
+
+    modular_seed_oss.SeedOssForCausalLM.forward = cce_forward
+    return None
